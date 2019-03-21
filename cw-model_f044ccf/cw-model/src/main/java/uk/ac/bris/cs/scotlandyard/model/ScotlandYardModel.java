f121fdc772af1dc13a487b0697841b30319d90ca
Kim <un18805@bristol.ac.uk>
2019-03-21 18:25:27 +0000

    'kc-21/03'

--- cw-model_f044ccf/cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ cw-model_f044ccf/cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -25,41 +25,57 @@ import uk.ac.bris.cs.gamekit.graph.Graph;
 
 // TODO implement all methods and pass all tests
 public class ScotlandYardModel implements ScotlandYardGame {
-	List<Boolean> rounds;
-	Graph<Integer, Transport> graph;
-	PlayerConfiguration mrX;
-	PlayerConfiguration firstDetective;
-	PlayerConfiguration restOfTheDetectives;
+
+	private List<Boolean> rounds;
+	private Graph<Integer, Transport> graph;
+	private PlayerConfiguration mrX;
+	private PlayerConfiguration firstDetective;
+	private PlayerConfiguration restOfTheDetectives;
+
 	public ScotlandYardModel(List<Boolean> rounds, Graph<Integer, Transport> graph,
 			PlayerConfiguration mrX, PlayerConfiguration firstDetective,
 			PlayerConfiguration... restOfTheDetectives) {
 		// TODO
+		//ensure rounds and graph cannot be null
 		this.rounds = requireNonNull(rounds);
 		this.graph = requireNonNull(graph);
+
+		//ensure that rounds is not empty
 		if (rounds.isEmpty()) {
 			throw new IllegalArgumentException("Empty rounds");
 		}
+
+		//ensure that graph is not empty
 		if(graph.isEmpty()) {
 			throw new IllegalArgumentException("Empty graph");
 		}
+
+		//ensure that MrX is black
 		if (mrX.colour != BLACK) { // or mr.colour.isDetective()
 			throw new IllegalArgumentException("MrX should be Black");
 		}
+
+		//put players into temporary list so that we can perform checks
 		ArrayList<PlayerConfiguration> configurations = new ArrayList<>();
 		for (PlayerConfiguration configuration : restOfTheDetectives)
 			configurations.add(requireNonNull(configuration));
 		configurations.add(0, firstDetective);
 		configurations.add(0, mrX);
-		Set<Integer> set = new HashSet<>();
-		//Set<Integer> set2 = new HashSet<>();
+
+		//iterate over list checking that locations and colours are not duplicated
+		Set<Integer> set1 = new HashSet<>();
+		Set<Colour> set2 = new HashSet<>();
 		for (PlayerConfiguration configuration : configurations) {
-			if (set.contains(configuration.location))
+			if (set1.contains(configuration.location))
 				throw new IllegalArgumentException("Duplicate location");
-			set.add(configuration.location);
-			//if(set.contains(configuration.colour))
-			//	throw new IllegalArgumentException("Duplicate colour");
-			//set.add(configuration.colour);
+			set1.add(configuration.location);
+			if(set2.contains(configuration.colour))
+				throw new IllegalArgumentException("Duplicate colour");
+			set2.add(configuration.colour);
+			if((configuration.tickets.containsKey(SECRET) || configuration.tickets.containsKey(DOUBLE)) && (configuration.colour != BLACK))
+				throw new IllegalArgumentException("Detective holds secret card");
 		}
+
 	}
 
 	@Override
